8d856a5ab43bbb88626cb5a2725325a39d04df97
Rashmi R <rashmi.shantha.r@gmail.com>
2021-08-10 15:44:09 +0530

    initial commit

---
+++ CoachEd_flashhired/CoachEd_flashhired/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CoachEd_flashhired project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoachEd_flashhired.settings')
+
+application = get_wsgi_application()
