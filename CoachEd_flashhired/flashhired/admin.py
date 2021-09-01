7a3da38db0f0d711c37849efa4dd55d8e2f5ade0
Rashmi R <rashmi.shantha.r@gmail.com>
2021-09-01 18:54:26 +0530

    Co-authored-by: skvinay <skvinay@users.noreply.github.com>

--- CoachEd_flashhired/flashhired/admin.py
+++ CoachEd_flashhired/flashhired/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
 admin.site.site_header = 'flashHired Administration' 
 #username = admin
 #pw: CoachEdINAHAKPK.flashired21
+
+admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','email']
--- CoachEd_flashhired/flashhired/views.py
+++ CoachEd_flashhired/flashhired/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,redirect
+from .forms import SignupForm,LoginForm
+from django.contrib.auth import authenticate,login
 # Create your views here.
 
 def pilot(request):
@@ -9,12 +9,40 @@ def pilot(request):
 def rechome(request):
     return render(request,'rechome.html',{})
 
-def login(request):
-    return render(request,'login.html',{})
+def login_view(request):
+    msg=None
+    form = LoginForm(request.POST or None)
+    if request.method=='POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                msg='invalid credentials'
+                print(msg)
+        else:
+            msg='error validating form'
+    return render(request,'login.html',{'form':form,'msg':msg})
 
 def SignupChoice(request):
     return render(request,'SignupChoice.html',{})
 
-
 def Signup(request):
-    return render(request,'Signup.html',{})
+    msg=None
+    if request.method=='POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg='user created successfully'
+            return redirect('login_view')
+        else: 
+            msg='form is not valid'
+    else: 
+        form = SignupForm() 
+    return render(request,'Signup.html',{'form':form,'msg':msg})
+
+def home(request):
+    return render(request,'home.html',{})
