b27b7b3b52ed020ec2e25ffbc6364fae89fd0ea3
Rashmi R <rashmi.shantha.r@gmail.com>
2021-09-22 14:14:55 +0530

    mongodb added

--- CoachEd_flashhired/CoachEd_flashhired/settings.py
+++ CoachEd_flashhired/CoachEd_flashhired/settings.py
@@ -86,10 +86,19 @@ WSGI_APPLICATION = 'CoachEd_flashhired.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'django-mongo',
+        'CLIENT': {
+           'host': 'mongodb+srv://rashmi:admin-123@django-mongo.skbct.mongodb.net/test',
+        }
     }
 }
 
