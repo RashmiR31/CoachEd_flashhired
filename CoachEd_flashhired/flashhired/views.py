acade0247d6b70f31db6518948a0f52b1cf268fc
Rashmi R <rashmi.shantha.r@gmail.com>
2021-09-23 11:37:25 +0530

    Password_reset bug fixed with customized email message

--- CoachEd_flashhired/flashhired/views.py
+++ CoachEd_flashhired/flashhired/views.py
@@ -3,6 +3,14 @@ from .forms import SignupForm,LoginForm
 from django.contrib.auth import authenticate,login,logout
 from .models import User
 from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
+from django.contrib.auth.forms import PasswordResetForm
+
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 # Create your views here.
 
 def pilot(request):
@@ -75,3 +83,32 @@ def RecruiterHome(request):
 def logout_view(request):
     logout(request)
     return render(request,'account/logout.html')
+
+def password_reset_request(request):
+    if request.method=="POST":
+        password_reset_form=PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data= password_reset_form.cleaned_data['email']
+            associated_users = User.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    subject = "Password Reset Requested"
+                    email_template_name = "account/password_reset_email.txt"
+                    c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+                    email=render_to_string(email_template_name,c)
+                    try:
+                        send_mail(subject,email,'',[user.email],fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse("Invalid header found.")
+                    return redirect('password_reset_done')
+    password_reset_form=PasswordResetForm()
+
+    return render(request,'account/password_reset.html',{"form":password_reset_form})
